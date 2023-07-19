937ed9dfb42626816e88661b34932f3876ffe491
jainharshul <harshuljainwork@gmail.com>
2023-07-18 22:25:04 -0700

    completed wih image that was given gives output in terminal

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import easyocr
+from PIL import Image
+from pytesseract import pytesseract
+import enum
 
-image_path = 'image.jpeg'
 
-reader = easyocr.Reader(['en'], gpu = False)
-result = reader.readtext(image_path)
-print(result)
+class OS(enum.Enum):
+    Mac = 0
+    Windows = 1
+
+
+class Language(enum.Enum):
+    ENG = 'eng'
+
+
+class ImageReader:
+
+    def __init__(self, os: OS):
+        if os == OS.Mac:
+            print('Running on Mac\n')
+
+    def extract_text(self, image: str, lang: str) -> str:
+        img = Image.open(image)
+        extracted_text = pytesseract.image_to_string(img, lang= lang)
+        return extracted_text
+
+
+if __name__ == '__main__':
+    ir = ImageReader(OS.Mac)
+    text = ir.extract_text('image/image.jpeg', lang = 'eng')
+    print(text)
